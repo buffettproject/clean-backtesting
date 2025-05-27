ac0cd11bff74cf80e00f16101e281bcdd85e25a7
SIC98 <shininchul98@gmail.com>
2025-05-27 15:50:30 +0900

    Add basisguard result

--- src/ywcho/basisguard.py
+++ src/ywcho/basisguard.py
@@ -77,12 +77,12 @@ class Basisguard(Strategy):
         spread_mean = spreads.mean()
 
         avg_funding_rate = self.funding_rate["funding_rate"].tail(30).mean()
-        funding_rate = self.funding_rate["funding_rate"].last()
+        last_funding_rate = self.funding_rate["funding_rate"].last()
 
         price_change_8h = self.um["close"].last() - self.um["close"][-2]
 
         if spread < spread_mean and (
-            funding_rate < 0 or funding_rate < avg_funding_rate
+            last_funding_rate < 0 or last_funding_rate < avg_funding_rate
         ):
             self.buy()
 
@@ -106,3 +106,39 @@ equity_curve = stats["_equity_curve"]
 
 print(stats)
 bt.plot()
+
+# Start                     2020-01-01 00:00:00
+# End                       2025-05-25 09:03:00
+# Duration                   1971 days 09:03:00
+# Exposure Time [%]                    22.34531
+# Equity Final [$]               195524212.9985
+# Equity Peak [$]                210248217.9625
+# Return [%]                           95.52421
+# Buy & Hold Return [%]              1391.73679
+# Return (Ann.) [%]                    13.21362
+# Volatility (Ann.) [%]                27.95412
+# CAGR [%]                             13.21806
+# Sharpe Ratio                          0.47269
+# Sortino Ratio                         1.06947
+# Calmar Ratio                          0.45593
+# Alpha [%]                            34.31547
+# Beta                                  0.04398
+# Max. Drawdown [%]                   -28.98175
+# Avg. Drawdown [%]                      -1.012
+# Max. Drawdown Duration      651 days 03:27:00
+# Avg. Drawdown Duration        5 days 14:25:00
+# # Trades                                 2640
+# Win Rate [%]                         38.59848
+# Best Trade [%]                       14.84495
+# Worst Trade [%]                      -2.02662
+# Avg. Trade [%]                        0.02523
+# Max. Trade Duration           0 days 08:00:00
+# Avg. Trade Duration           0 days 04:00:00
+# Profit Factor                         1.08856
+# Expectancy [%]                        0.03096
+# SQN                                   1.39935
+# Kelly Criterion                       0.02903
+# _strategy                          Basisguard
+# _equity_curve                             ...
+# _trades                          Size  Ent...
+# dtype: object
